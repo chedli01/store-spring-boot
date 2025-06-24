4385ea65e09c6ac6a8d86a346b1b621e73325e43
chedli01 <chedli.masmoudi01@gmail.com>
2025-06-24 14:54:55 +0100

    change controller

--- src/main/java/com/coding/store/HomeController.java
+++ src/main/java/com/coding/store/HomeController.java
@@ -1,16 +1,13 @@
 package com.coding.store;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/home")
+@Controller
 public class HomeController {
-    @GetMapping("/hi")
+    @RequestMapping("/")
     public String index(){
-        return "this is chedli";
+        return "index.html";
     }
 
 }
